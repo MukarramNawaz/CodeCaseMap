11b2b8b3443b26cd461813a08b05605009ecf520
Mukarram Nawaz <156927769+MukarramNawaz@users.noreply.github.com>
2025-05-03 20:48:04 +0500

    Clear current chat and create a new chat upon deleting an active thread

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -278,6 +278,11 @@ function Chat() {
     try {
       await deleteConversationById(id);
 
+      // Clear current chat and create new chat if the deleted thread is open
+      if (currentThreadId === id) {
+        createNewChat(); // This will clear messages, thread ID and prep for new chat
+      }
+
       setChatHistory((prev) =>
         Object.fromEntries(
           Object.entries(prev).map(([key, chats]) => [
@@ -320,6 +325,7 @@ function Chat() {
     setMessage("");
     setMessages([]);
     setCurrentThreadId(null);
+    
     // setIsSidebarOpen(false);
   };
 
@@ -341,173 +347,7 @@ function Chat() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  //     try {
-  //       const { data } = await createConversation(message);
-  //       setChatHistory((prev) => ({
-  //         ...prev,
-  //         today: [data, ...prev.today],
-  //       }));
-
-  //       setCurrentThreadId(data.id);
-
-  //       if (message.trim()) {
-  //         const userMessage = message.trim();
-  //         setMessage("");
-  //         setMessages((prev) => [
-  //           ...prev,
-  //           { content: userMessage, role: "user" },
-  //         ]);
-
-  //         try {
-  //           const response = await sendMessageToConversation(
-  //             currentThreadId,
-  //             message);
-
-  //           const reader = response.body.getReader();
-  //           const decoder = new TextDecoder();
-  //           let assistantMessage = { content: "", role: "assistant" };
-
-  //           while (true) {
-  //             const { done, value } = await reader.read();
-  //             if (done) break;
-
-  //             const chunk = decoder.decode(value, { stream: true });
-  //             const lines = chunk.split("\n");
-
-  //             for (const line of lines) {
-  //               if (line.trim() === "") continue;
-
-  //               const parsedLine = JSON.parse(line);
-  //               if (parsedLine.event === "delta") {
-  //                 assistantMessage.content += parsedLine.data;
-  //                 setMessages((prev) => {
-  //                   const lastMessage = prev[prev.length - 1];
-  //                   if (lastMessage.role === "assistant") {
-  //                     return [
-  //                       ...prev.slice(0, -1),
-  //                       { ...lastMessage, content: assistantMessage.content },
-  //                     ];
-  //                   } else {
-  //                     return [...prev, assistantMessage];
-  //                   }
-  //                 });
-  //               } else if (parsedLine.event === "sources") {
-  //                 // Update the assistant's message with the sources
-  //                 assistantMessage.sources = parsedLine.data;
-  //                 setMessages((prev) => {
-  //                   const lastMessage = prev[prev.length - 1];
-  //                   if (lastMessage.role === "assistant") {
-  //                     return [
-  //                       ...prev.slice(0, -1),
-  //                       { ...lastMessage, sources: assistantMessage.sources },
-  //                     ];
-  //                   } else {
-  //                     return [...prev, assistantMessage];
-  //                   }
-  //                 });
-  //               } else if (parsedLine.event === "done") {
-  //                 // Update the assistant's message with the sources
-  //                 assistantMessage.id = parsedLine.message_id;
-  //                 setMessages((prev) => {
-  //                   const lastMessage = prev[prev.length - 1];
-  //                   if (lastMessage.role === "assistant") {
-  //                     return [
-  //                       ...prev.slice(0, -1),
-  //                       { ...lastMessage, id: assistantMessage.id },
-  //                     ];
-  //                   } else {
-  //                     return [...prev, assistantMessage];
-  //                   }
-  //                 });
-  //               }
-  //             }
-  //           }
-  //         } catch (error) {
-  //           toast.error("Failed to send message");
-  //         } finally {
-  //           setIsLoading(false);
-  //         }
-  //       }
-  //       return;
-  //     } catch (error) {
-  //       toast.error("Failed to send message");
-  //       setIsLoading(false);
-  //       return;
-  //     }
-  //   }
-
-  //   if (message.trim() && !isLoading) {
-  //     const userMessage = message.trim();
-  //     setMessage("");
-  //     setMessages((prev) => [...prev, { content: userMessage, role: "user" }]);
-
-  //     try {
-  //       const response = await fetch(
-  //         `${API_BASE_URL}/user/conversation/${currentThreadId}/message/send`,
-  //         {
-  //           method: "POST",
-  //           headers: {
-  //             Accept: "application/json",
-  //             "Content-Type": "application/json",
-  //           },
-  //           credentials: "include", // Equivalent to `withCredentials: true` in Axios
-  //           body: JSON.stringify({ message, stream: true }),
-  //         }
-  //       );
-
-  //       const reader = response.body.getReader();
-  //       const decoder = new TextDecoder();
-  //       let assistantMessage = { content: "", role: "assistant" };
-
-  //       while (true) {
-  //         const { done, value } = await reader.read();
-  //         if (done) break;
-
-  //         const chunk = decoder.decode(value, { stream: true });
-  //         const lines = chunk.split("\n");
-
-  //         for (const line of lines) {
-  //           if (line.trim() === "") continue;
-
-  //           const parsedLine = JSON.parse(line);
-  //           if (parsedLine.event === "delta") {
-  //             assistantMessage.content += parsedLine.data;
-  //             setMessages((prev) => {
-  //               const lastMessage = prev[prev.length - 1];
-  //               if (lastMessage.role === "assistant") {
-  //                 return [
-  //                   ...prev.slice(0, -1),
-  //                   { ...lastMessage, content: assistantMessage.content },
-  //                 ];
-  //               } else {
-  //                 return [...prev, assistantMessage];
-  //               }
-  //             });
-  //           } else if (parsedLine.event === "sources") {
-  //             // Update the assistant's message with the sources
-  //             assistantMessage.sources = parsedLine.data;
-  //             setMessages((prev) => {
-  //               const lastMessage = prev[prev.length - 1];
-  //               if (lastMessage.role === "assistant") {
-  //                 return [
-  //                   ...prev.slice(0, -1),
-  //                   { ...lastMessage, sources: assistantMessage.sources },
-  //                 ];
-  //               } else {
-  //                 return [...prev, assistantMessage];
-  //               }
-  //             });
-  //           }
-  //         }
-  //       }
-  //     } catch (error) {
-  //       toast.error("Failed to send message");
-  //     } finally {
-  //       setIsLoading(false);
-  //     }
-  //   }
-  // };
-
+  
   const handleSendMessage = async (e) => {
     e.preventDefault();
     const text = message.trim();
